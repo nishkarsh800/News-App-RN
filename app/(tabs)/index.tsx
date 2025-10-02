0a022ef075c1b475c4e26bb9b97e7ec764ddbcfb
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2025-10-02 10:30:24 +0530

    feat: Impl the newsList section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import BreakingNews from '@/components/BreakingNews';
+import Categories from '@/components/Categories';
 import Header from '@/components/Header';
+import NewsList from '@/components/NewsList';
 import SearchBar from '@/components/SearchBar';
 import { NewsDataType } from '@/types';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'; // ✅ import useState
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type Props = {}
@@ -12,15 +14,18 @@ type Props = {}
 const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets()
   const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([])
+  const [news, setNews] = useState<NewsDataType[]>([])
+
 
   useEffect(() => {
     getBreakingNews()  // called the breakingNews()
+    getNews()
   }, [])
   
 
   const getBreakingNews = async () => {
     try {
-      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=5`
       const response = await axios.get(URL)
 
       if (response && response.data) {
@@ -29,14 +34,35 @@ const Page = (props: Props) => {
     } catch (error) {
       console.error(error)
     }
+  };
+
+  const getNews = async () => {
+  try {
+    const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10`
+    const response = await axios.get(URL)
+
+    if (response && response.data) {
+      setNews(response.data.results)  
+    }
+  } catch (error) {
+    console.error(error)
   }
+};
+
+
+  const onCatChanged = (category: string)=>{
+    console.log('Category: ', category);
+
+  };
 
   return (
-    <View style={[styles.container, { paddingTop: safeTop }]}>
+    <ScrollView style={[styles.container, { paddingTop: safeTop }]}>
       <Header />
       <SearchBar />
       <BreakingNews newsList={breakingNews}/>
-    </View>
+      <Categories onCategoryChanged={onCatChanged}/>
+      <NewsList newsList={news}/>
+    </ScrollView>
   )
 }
 
---
+++ components/NewsList.tsx
@@ -0,0 +1,83 @@
+import { Colors } from '@/constants/Colors'
+import { NewsDataType } from '@/types'
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+
+type Props = {
+    newsList : Array<NewsDataType>
+}
+
+const NewsList = ({newsList}: Props) => {
+  return (
+    <View
+    style = {styles.container}>
+      {newsList.map((item, index) => (
+        <View key={index} style = {styles.itemConatiner}>
+            <Image source={{uri: item.image_url}} style ={styles.itemImage} />
+            <View style= {styles.itemInfo}>
+                <Text style ={styles.itemCategory}>{item.category}</Text>
+                <Text style ={styles.itemTitle}>{item.title}</Text>
+                <View style={styles.itemSourceInfo}>
+                    <Image source={{uri: item.source_icon}} style={styles.itemSourceImage}/>
+                    <Text style={styles.itemSourceName}>{item.source_name}</Text>
+                </View>
+            </View>
+        </View>
+      ))}
+    </View>
+  )
+}
+
+export default NewsList
+
+const styles = StyleSheet.create({
+    container:{
+        marginHorizontal: 20,
+        marginBottom: 50
+
+    },
+    itemConatiner:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+        flex: 1,
+        gap: 10
+    },
+    itemImage:{
+        width: 90,
+        height: 100,
+        borderRadius: 20,
+        marginRight : 10
+    },
+    itemInfo:{
+        flex :1,
+        gap: 10,
+        justifyContent: 'space-between',
+    },
+    itemCategory:{
+        fontSize: 12,
+        color: Colors.darkGrey,
+        textTransform: 'capitalize',
+
+    },
+    itemTitle:{
+        fontSize : 12,
+        fontWeight: '600',
+        color: Colors.black
+    },
+    itemSourceInfo:{
+        flexDirection:'row',
+        gap: 8,
+        alignItems: 'center',
+    },
+    itemSourceImage:{
+        width: 20,
+        height: 20,
+        borderRadius: 20,
+    },
+    itemSourceName:{
+        fontSize: 10,
+        fontWeight: '400',
+        color: Colors.darkGrey
+    }
+})
