39e2c9e1d95b44929c27c5c9008dff9d610e3ca1
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2025-10-05 16:28:09 +0530

    feat : displaying saved news in saved screen

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,13 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import Loading from '@/components/Loading'
+import NewsItem from '@/components/NewsItem'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useIsFocused } from '@react-navigation/native'
+import axios from 'axios'
+import { Link, Stack } from 'expo-router'
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, TouchableOpacity, View } from 'react-native'
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const [bookmarkNews, setBookmarkNews] = useState([])
+  const [isLoading, setisLoading] = useState(true)
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    fetchBookmark()
+  }, [isFocused]);
+  
+  const fetchBookmark = async() => {
+    await AsyncStorage.getItem('bookmark').then(async(token) => {
+      const res = JSON.parse(token);
+      setisLoading(true);
+      if(res){
+        console.log('Bookmark res: ', res);
+        let query_string = res.join(',');
+        console.log('Query string: ',query_string);
+
+        const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${query_string}`)
+        setBookmarkNews(response.data.results);
+        setisLoading(false);
+      }else{
+        setBookmarkNews([])
+        setisLoading(false)
+      }
+    })
+  }
   return (
+    <>
+    <Stack.Screen options={{
+      headerShown:  true,
+
+    }} />
     <View style={styles.container}>
-      <Text>Saved Screen</Text>
+      {isLoading ?(
+        <Loading size={'large'}/>
+      ):(
+        <FlatList
+            data={bookmarkNews}
+            keyExtractor={(_, index) => `list_items${index}`}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ index,item }) => (
+                <Link href={`/news/${item.article_id}`} asChild key={index}>
+                    <TouchableOpacity>
+                       <NewsItem item={item}/> 
+                    </TouchableOpacity>
+                </Link>
+            )}
+          />
+      )}
     </View>
+    </>
   )
 }
 
@@ -16,7 +69,6 @@ export default Page
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    margin: 20
   },
 })
